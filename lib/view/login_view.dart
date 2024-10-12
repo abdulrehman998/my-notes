c362bc68ad8402b84eb32a0909479a15e7ac1bf4
Saad3954 <saad.asad898@gmail.com>
2024-10-12 16:43:28 +0500

    first commit

---
+++ lib/view/login_view.dart
@@ -0,0 +1,91 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mynotes/firebase_options.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login Page')),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return Column(
+                children: [
+                  TextField(
+                    controller: _email,
+                    autocorrect: false,
+                    enableSuggestions: false,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(hintText: 'Enter your Email'),
+                  ),
+                  TextField(
+                    controller: _password,
+                    obscureText: true,
+                    autocorrect: false,
+                    enableSuggestions: false,
+                    decoration:
+                        InputDecoration(hintText: 'Enter your Password'),
+                  ),
+                  TextButton(
+                      onPressed: () async {
+                        final email = _email.text;
+                        final password = _password.text;
+
+                        try {
+                          final userCredebtail = await FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
+                                  email: email, password: password);
+
+                          print(userCredebtail);
+                        } on FirebaseAuthException catch (e) {
+                          print(e.code);
+
+                          if (e.code == 'invalid-credential') {
+                            print('Wrong Credentials');
+                          } else if (e.code == 'too-many-requests') {
+                            print('Wrong Password');
+                          }
+                        }
+                      },
+                      child: const Text('Login')),
+                ],
+              );
+            default:
+              return Text('Loading..');
+          }
+        },
+      ),
+    );
+  }
+}
